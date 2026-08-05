3493f798586fb5e528d9c9e3c857840871f0cef1
lollolhappy826-lgtm <lollolhappy826@gmail.com>
2026-08-05 11:31:44 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Pisergisi Empire Website
+// ==========================
+// PISERGISI EMPIRE
+// Official Portal
+// ==========================
+
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    if(window.scrollY > 80){
+        header.style.background = "rgba(7,18,34,.92)";
+    }else{
+        header.style.background = "rgba(7,18,34,.65)";
+    }
+
+});
